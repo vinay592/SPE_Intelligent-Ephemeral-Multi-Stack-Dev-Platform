709fc7183610f1f59e3c8dd4f90c031d580f6307
Vinay <vinayvb592@gmail.com>
2026-03-28 15:39:14 +0530

    Fixed backend restart issue for dynamic env API

--- backend/app.py
+++ backend/app.py
@@ -7,18 +7,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Dev Platform Backend Running"
+    return "Dev Platform Backend Running 🚀"
+
 
 @app.route('/create-env', methods=['POST'])
 def create_env():
     data = request.json
 
     stack = data.get("stack")
-    cpu = data.get("cpu")
-    memory = data.get("memory")
+    cpu = data.get("cpu", "500m")
+    memory = data.get("memory", "512Mi")
 
     env_id = str(uuid.uuid4())[:6]
 
+    #  Stack Mapping
+    if stack == "flask":
+        image = "flask-env"
+        port = 5001
+
+    elif stack == "mern":
+        image = "mern-env"
+        port = 3000
+
+    elif stack == "java":
+        image = "java-env"
+        port = 8082
+
+    elif stack == "ml":
+        image = "ml-env"
+        port = 8888
+
+    else:
+        return jsonify({"error": "Invalid stack"}), 400
+
+    #  Dynamic Deployment YAML
     deployment_yaml = f"""
 apiVersion: apps/v1
 kind: Deployment
@@ -35,35 +57,61 @@ spec:
         app: env-{env_id}
     spec:
       containers:
-      - name: flask-container
-        image: flask-env
+      - name: app-container
+        image: {image}
         imagePullPolicy: Never
         ports:
-        - containerPort: 5001
+        - containerPort: {port}
         resources:
           limits:
             memory: "{memory}"
             cpu: "{cpu}"
 """
 
-    file_path = f"/tmp/env-{env_id}.yaml"
+    # 🔥 Dynamic Service YAML
+    node_port = 30000 + int(env_id[:3], 16) % 2000
 
-    with open(file_path, "w") as f:
+    service_yaml = f"""
+apiVersion: v1
+kind: Service
+metadata:
+  name: svc-{env_id}
+spec:
+  type: NodePort
+  selector:
+    app: env-{env_id}
+  ports:
+    - port: 80
+      targetPort: {port}
+      nodePort: {node_port}
+"""
+
+    # Save files
+    dep_file = f"/tmp/deploy-{env_id}.yaml"
+    svc_file = f"/tmp/service-{env_id}.yaml"
+
+    with open(dep_file, "w") as f:
         f.write(deployment_yaml)
 
-    os.system(f"kubectl apply -f {file_path}")
+    with open(svc_file, "w") as f:
+        f.write(service_yaml)
+
+    # Apply to Kubernetes
+    os.system(f"kubectl apply -f {dep_file}")
+    os.system(f"kubectl apply -f {svc_file}")
 
     return jsonify({
         "env_id": env_id,
-        "status": "created"
+        "stack": stack,
+        "status": "created",
+        "access_port": node_port
     })
 
 
 @app.route('/delete-env/<env_id>', methods=['DELETE'])
 def delete_env(env_id):
-    print(f"Deleting environment {env_id}")
-
-    os.system("kubectl delete deployment flask-env")
+    os.system(f"kubectl delete deployment env-{env_id}")
+    os.system(f"kubectl delete service svc-{env_id}")
 
     return jsonify({
         "env_id": env_id,
